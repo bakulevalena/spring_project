53741e88d727f1cd48866fa566660a32fb936e01
bakulevalena <bakuleva.lena@gmail.com>
2019-09-23 00:27:36 +0200

    add dto, entity and repository. Some methods was rewrited

---
+++ src/main/java/com/example/homework/entity/Message.java
@@ -0,0 +1,39 @@
+package com.example.homework.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class Message {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(name = "message_text")
+    private String message;
+    @Column(name = "creation_date")
+    private LocalDateTime creationDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+}
--- src/main/java/com/example/homework/service/MessageProcessingService.java
+++ src/main/java/com/example/homework/service/MessageProcessingService.java
@@ -1,6 +1,8 @@
 package com.example.homework.service;
 
 import com.example.homework.dto.MessageDTO;
+import com.example.homework.entity.Message;
+import com.example.homework.repository.MessageRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -8,18 +10,30 @@ import java.time.LocalDateTime;
 @Service
 public class MessageProcessingService implements PayloadService {
 
-    private String savedMessage;
+    private MessageRepository messageRepository;
+
+    public MessageProcessingService(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
 
     @Override
     public String processMessage(MessageDTO message) {
-        this.savedMessage = message.getMessage();
+        Message savedMessage = new Message();
+        savedMessage.setMessage(message.getMessage());
+        savedMessage.setCreationDate(LocalDateTime.now());
+        messageRepository.save(savedMessage);
         return "Saved";
     }
 
     @Override
     public MessageDTO getMessage() {
         MessageDTO messageDTO = new MessageDTO();
-        messageDTO.setMessage(savedMessage);
+        Message lastMessage = messageRepository.findFirstByMessageIsNotNullOrderByCreationDateDesc();
+        if (lastMessage != null) {
+            messageDTO.setMessage(lastMessage.getMessage());
+        } else {
+            return null;
+        }
         return messageDTO;
     }
 
